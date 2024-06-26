07abb5a1a3bcb12f1ab7ce7e5c99cc885030a2dd
Jan Schwenzien <jan@general-scripting.com>
2024-06-26 16:41:51 +0200

    green test, LINTER

--- lib/skynet/client.rb
+++ lib/skynet/client.rb
@@ -1,17 +1,20 @@
 # frozen_string_literal: true
 
+# require_relative "../skynet"
+require "httparty"
+
 module Skynet
   class Client
     include HTTParty
-    base_uri ::Skynet.config.base_uri
 
     headers "Content-Type" => "application/json"
     format :json
 
     raise_on [400, 401, 406, 422, 500]
 
-    def initialize(secret: ::Skynet.config.secret, iss: ::Skynet.config.app_id)
-      self.class.headers "Authorization" => "jwt #{jwt_token(secret:, iss:)}" if secret.present? && iss.present?
+    def initialize(secret: Skynet.config.secret, iss: Skynet.config.app_id)
+      self.class.base_uri Skynet.config.base_uri
+      self.class.headers "Authorization" => "jwt #{jwt_token(secret: secret, iss: iss)}" if secret.present? && iss.present?
     end
 
     def agent_session(agent_session_id)
@@ -19,7 +22,7 @@ module Skynet
     end
 
     def upload(file)
-      self.class.post("/uploads", body: { file: file })
+      self.class.post("/uploads", body: {file: file})
     end
 
     def download(uuid)
@@ -27,8 +30,8 @@ module Skynet
     end
 
     def jwt_token(secret:, iss:, exp: 4.hours.from_now.to_i)
-      payload = { iss:, exp: }
-      header_fields = { alg: "HS256" }
+      payload = {iss: iss, exp: exp}
+      header_fields = {alg: "HS256"}
       JWT.encode payload, secret, "HS256", header_fields
     end
   end
--- lib/skynet/configuration.rb
+++ lib/skynet/configuration.rb
@@ -3,20 +3,5 @@
 module Skynet
   class Configuration
     attr_accessor :base_uri, :app_id, :secret
-
-    def initialize
-    end
-
-    def base_uri(uri)
-      @base_uri = uri
-    end
-
-    def app_id(id)
-      @app_id = id
-    end
-
-    def secret(secret)
-      @secret = secret
-    end
   end
 end
